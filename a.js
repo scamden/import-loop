0d48c80f6677d8be63ed6d63df5141a619effefe
Sterling Camden <rusty.camden@gmail.com>
2022-10-19 10:36:02 -0700

    fix(build): no build artifacts

--- a.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.aThing = void 0;
-require("./c");
-var b_1 = require("./b");
-var aThing = function () { return 'blah'; };
-exports.aThing = aThing;
-console.log(b_1.cThing);
